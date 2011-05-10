c049432216e29f3376f10f5997ebdaac046dc945
Ross McFarland <rmcfarland@saymedia.com>
2011-05-10 10:35:11 -0700

    initial import/commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='webassets-dynamic',
+    version='1.0',
+    packages=['webassets_dynamic'],
+    requires=['webassets'],
+)
---
+++ webassets_dynamic/__init__.py
@@ -0,0 +1,39 @@
+from django.conf.urls.defaults import RegexURLPattern, RegexURLResolver
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.views.static import serve
+from django_assets import Bundle, register
+from webassets.filter import Filter, register_filter
+from os.path import join
+import sys
+
+class URLPatternHelper:
+    names_to_paths = {}
+
+    def __init__(self):
+        urls = __import__(settings.ROOT_URLCONF, globals(), locals(), 
+                ['urlpatterns'], -1) 
+        for url_pattern in urls.urlpatterns:
+            self._walk(url_pattern)
+
+    def _walk(self, url_pattern):
+        if isinstance(url_pattern, RegexURLResolver):
+            for url_pattern in url_pattern.url_patterns:
+                self._walk(url_pattern)
+        elif isinstance(url_pattern, RegexURLPattern):
+            if url_pattern.callback == serve:
+                self.names_to_paths[url_pattern.name] = \
+                        url_pattern.default_args['document_root']
+
+    def get_path(self, name):
+        return self.names_to_paths[name]
+
+helper = URLPatternHelper()
+
+# TODO: should find a better way to get at this
+mode = sys.argv[1]
+def dynamic_assets(name, file):
+    if mode == 'assets':
+        return join(helper.get_path(name), file)
+    else:
+        return reverse(name, args=[file])
